dd7a1517e82d899c5594be1db93f8ba769093b8d
Tim Hall <timothypatrickhall@gmail.com>
2015-10-30 02:13:43 +0000

    Create unsuccessful author feature

--- spec/features/author_feature_spec.rb
+++ spec/features/author_feature_spec.rb
@@ -13,4 +13,18 @@ RSpec.feature "Creating Authors" do
         
         expect(page).to have_content("Author has been created.")
     end
+    
+     scenario "with an invalid input fails" do
+        visit root_path
+        
+        click_link "Authors"
+        click_link "Add new author"
+        
+        fill_in "First name", with: ""
+        fill_in "Last name", with: ""
+        
+        click_button "Create Author"
+        
+        expect(page).to have_content("Author has not been created.")
+    end
 end
