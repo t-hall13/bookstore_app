774d2ae7c45525e8c4348c6b66775defa7e0042b
Tim Hall <timothypatrickhall@gmail.com>
2015-11-09 21:39:26 +0000

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Author.create!(first_name: 'Agatha', last_name: 'Christie')
-Author.create!(first_name: 'James', last_name: 'Chase')
-Author.create!(first_name: 'Sydney', last_name: 'Sheldon')
+Author.create!(first_name: 'Bram', last_name: 'Stoker')
+Author.create!(first_name: 'E.', last_name: 'Motion')
 Author.create!(first_name: 'Obie', last_name: 'Fernandez')
 Author.create!(first_name: 'Cloves', last_name: 'Carneiro')
 Author.create!(first_name: 'Rida', last_name: 'Al Barazi')
@@ -27,3 +27,31 @@ Publisher.create!([
               awesome opensource plotting software', published_at:'2010-11-04',
               publisher_id: 4, 
               book_cover: File.open(File.join(Rails.root, 'app/assets/images/gnuplot.jpg')))
+              
+ Book.create!(title: 'Dracula', isbn: '989-1-933-98839-6',
+              page_count: 500, price: 66.37, description: 'The one and only awesome Vampire tale', published_at:'2010-11-04',
+              publisher_id: 4, 
+              book_cover: File.open(File.join(Rails.root, 'app/assets/images/Dracula_Book_Cover_1916.jpg')))
+              
+  Book.create!(title: 'Living Vicariously', isbn: '989-1-933-98849-9',
+              page_count: 375, price: 61.37, description: 'A book about living vicarioulsy',
+              published_at:'2010-11-04',
+              publisher_id: 4, 
+              book_cover: File.open(File.join(Rails.root, 'app/assets/images/Living_Vicariously.jpg')))            
+            
+  Book.create!(title: 'The Sisters Brothers', isbn: '949-1-933-98839-9',
+              page_count: 360, price: 66.37, description: 'A book about the sisters brothers', 
+              published_at:'2010-11-04',
+              publisher_id: 4, 
+              book_cover: File.open(File.join(Rails.root, 'app/assets/images/SistersBrothers.jpg')))
+              
+  Book.create!(title: 'None of Your Business', isbn: '989-1-953-98839-9',
+              page_count: 360, price: 66.37, description: 'Don\'t you dare read this!', published_at:'2010-11-04',
+              publisher_id: 4, 
+              book_cover: File.open(File.join(Rails.root, 'app/assets/images/none_of_your_business.gif')))
+              
+  Book.create!(title: 'This Book Has no Title', isbn: '989-1-933-98839-9',
+              page_count: 360, price: 66.37, description: 'Maybe you can come up with one! Hey,
+              let\'s hava a contest!', published_at:'2010-11-04',
+              publisher_id: 4, 
+              book_cover: File.open(File.join(Rails.root, 'app/assets/images/publication-187.jpg')))            
