9876f506f5b07053ddfc5caef5332ac15c758e37
Tim Hall <timothypatrickhall@gmail.com>
2015-11-11 21:47:11 +0000

    Validate user email format

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
   
-  validates :first_name, :last_name, :email, presence: true
+  validates :first_name, :last_name,  presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, 
+    format: { with: /\A[A-Z0-9#_-~!$&'()*+,;=:.%]+@[A-Z0-9.-]+\.[A-Z]{2,4}\z/i}
   
   def full_name
     "#{first_name} #{last_name}"
