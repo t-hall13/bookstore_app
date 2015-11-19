045144cf4af4bac25025cf5a0548580c77858535
Tim Hall <timothypatrickhall@gmail.com>
2015-11-19 14:36:21 +0000

    halfway through fixing author features

---
+++ spec/features/listing_authors_feature_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+require 'support/macros'
+RSpec.feature 'Listing Authors' do
+  let!(:john) { Fabricate(:author, first_name: 'John', last_name: 'Doe') }
+  let!(:mary) { Fabricate(:author, first_name: 'Mary', last_name: 'Grant') }
+  let(:admin) { Fabricate(:admin) }
+  
+  before { sign_in_as admin }
+  
+  
+  scenario "in the database" do
+    visit root_path
+   
+    within('.page-header') do
+       click_link 'Authors'
+      expect(page).to have_content('All Authors')
+    end
+    
+    within('#new-author-link') do
+      expect(page).to have_link('Add New Author')
+    end
+    
+    within('#authors-list') do
+      expect(page).to have_content('John Doe')
+      expect(page).to have_content('Mary Grant')
+    end
+  end
+  
+  scenario 'with none created' do
+    Author.delete_all
+    visit root_path
+    click_link 'Authors'
+    within('.page-header') do
+      expect(page).to have_content('No Authors Created')
+    end
+    
+    within('#new-author-link') do
+      expect(page).to have_link('Add New Author')
+    end
+    
+    within('#authors-list') do
+      expect(page).not_to have_content('John Doe')
+      expect(page).not_to have_content('Mary Grant')
+    end
+  end
+end
