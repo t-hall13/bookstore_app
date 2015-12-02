f240d2e3156b2cfb00069a19acc89fcf98355cf7
Tim Hall <timothypatrickhall@gmail.com>
2015-12-02 22:48:01 +0000

    Implement line total and total sale methods

---
+++ spec/features/creating_orders_feature_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+RSpec.feature "Creating orders" do
+  let!(:book) { Fabricate(:book) }
+  let!(:cart) { Fabricate(:cart) }
+  let(:user) { Fabricate(:user) }
+  before do 
+    sign_in_as user
+  end
+  
+  scenario 'creates a valid book' do
+    visit root_path
+    click_link 'Catalogs'
+    click_link book.title
+    click_button 'Add to cart'
+    click_button 'Checkout'
+    
+    fill_in 'Credit Card Number', with: '4242424242424242'
+    fill_in 'Security Code', with: '123'
+    select '10 - October', from: 'date_month'
+    select '2016', from: 'date_year'
+    click_button 'Create Order'
+    
+    expect(page).to have_content('Order has been created.')
+  end
+end
