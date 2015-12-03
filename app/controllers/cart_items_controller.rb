88032921daa79779fb4ddc9e90cd3d52248f3dfb
Tim Hall <timothypatrickhall@gmail.com>
2015-12-03 14:57:09 +0000

    Create cart with Ajax

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,9 +1,15 @@
 class CartItemsController < ApplicationController
     before_action :set_cart
-  def create
-      @cart_item = @cart.add_book_to_items(params[:book_id])
-      if @cart_item
-          redirect_to @cart_item.cart
-      end  
-  end
+    def create
+        cart_item = @cart.add_book_to_items(params[:book_id])
+        respond_to do |format|
+          if cart_item
+            format.html { redirect_to @cart_item.cart }
+            format.js
+          else
+            format.html { redirect_to catalogs_path }  
+          end 
+        end
+    end
 end
+  
